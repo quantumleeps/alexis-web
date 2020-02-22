574cc2592e26744f8c91cbcc7790733c60ef4d46
Daniel Leeper <dan@daniels-imac.home>
2020-02-22 12:08:01 -0500

    Minor update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import BackgroundImage from "./img/alexis_new.jpg"
+import BackgroundImage from "./img/alexis_000.jpg"
 
 export default () => (
   <Layout location="/" backgroundUrl={BackgroundImage}>
