8999d977677667cf6473bc60eeb3a898ec93116a
Daniel Leeper <dan@daniels-imac.home>
2020-06-15 19:57:15 -0400

    Added June 15th update

--- src/pages/course_materials.js
+++ src/pages/course_materials.js
@@ -1,8 +1,7 @@
 import React from "react";
 import BackgroundImage from "./img/tiling/012.png";
 import PayLogo from "./img/paypal.png";
-import Pic1 from "./img/camp1.jpg";
-import Pic2 from "./img/camp2.jpg";
+import Frame from "./img/frame.jpg";
 import Responsive from "react-responsive";
 
 const Desktop = (props) => <Responsive {...props} minWidth={767} />;
@@ -20,7 +19,6 @@ export default () => (
       overflow: `scroll`,
     }}
   >
-
     <div
       style={{
         backgroundColor: `white`,
@@ -34,7 +32,6 @@ export default () => (
       }}
     >
       <h2>Stage Meets Screen - Course Materials</h2>
-
       <br />
       <p>
         Please visit this page periodically. I'll be posting course materials
@@ -54,13 +51,46 @@ export default () => (
           color: `white`,
         }}
       >
+        June 15th: Find your frame!
+      </div>
+      <span style={{ marginLeft: "10px" }}>Click/tap on the image to enlarge it!</span>
+      <a
+        href="./static/frame-37663d3ce9f068eb45c341aea4ba736e.jpg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Desktop>
+          <img style={{ margin: `12px`, maxHeight: `800px` }} src={Frame} />
+        </Desktop>
+        <Mobile>
+          <img style={{ margin: `12px`, maxHeight: `600px` }} src={Frame} />
+        </Mobile>
+      </a>
+      <div
+        style={{
+          backgroundColor: `#8A73B9`,
+          opacity: `0.9`,
+          borderRadius: `6px`,
+          padding: `15px`,
+          margin: `15px`,
+          marginBottom: `25px`,
+          fontWeight: `bold`,
+          fontFamily: `Gill Sans Extrabold, sans-serif`,
+          color: `white`,
+        }}
+      >
         SMS Pledge:
       </div>
-      <p style={{ margin: '20px' }}>
-      I, ___insert camper name___ do solemnly promise to uphold the values of Stage Meets Screen Camp. I vow to be a supportive friend, a focused participant, and a respectful actor. I will be open to trying new things, to being silly, and to not taking anything too seriously. If something is challenging, I will remember that it just means that I’m growing. If something is confusing, I will be bold and ask questions. I will be kind to myself and others and always remember that I’m am here to have fun and progress as a performer and person!
-
+      <p style={{ margin: "20px" }}>
+        I, ___insert camper name___ do solemnly promise to uphold the values of
+        Stage Meets Screen Camp. I vow to be a supportive friend, a focused
+        participant, and a respectful actor. I will be open to trying new
+        things, to being silly, and to not taking anything too seriously. If
+        something is challenging, I will remember that it just means that I’m
+        growing. If something is confusing, I will be bold and ask questions. I
+        will be kind to myself and others and always remember that I’m am here
+        to have fun and progress as a performer and person!
       </p>
-
       <div
         style={{
           backgroundColor: `#8A73B9`,
@@ -133,7 +163,6 @@ export default () => (
           allowfullscreen
         ></iframe>
       </div>
-
     </div>
   </div>
 );
