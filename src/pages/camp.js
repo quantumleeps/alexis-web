f0b931cf6096b8a02f5ea95db689b228f4dc6e41
Daniel Leeper <dan@daniels-imac.home>
2020-07-24 19:44:38 -0400

    Revamp site for round 2

--- src/pages/camp.js
+++ src/pages/camp.js
@@ -9,20 +9,7 @@ const Desktop = (props) => <Responsive {...props} minWidth={767} />;
 const Mobile = (props) => <Responsive {...props} maxWidth={766} />;
 
 export default () => (
-  // <Layout location="/about" backgroundUrl={BackgroundImage} backgroundColor={"rgb(238, 181, 48)"}>
-  //   <div style={{ backgroundColor: `white`, padding: `20px`, opacity:  `0.65`, borderRadius: `6px`}} >
-  //     <h2>Biography</h2>
-  //     <p>Since weaseling her way into a community theatre production of Fiddler on the Roof at the tender age of 4, Alexis has maintained her passion for the spotlight. Her performances have ranged from the innocent to the profane with nothing identifiable in the center. Alexis has filmed commercials for both Percolate, Fidelity Investments, and the Food Network and made her feature film debut in Literally Right Before Aaron, written and directed by Ryan Eggold and starring Justin Long. She also starred in “The Office meets Hogwarts” web series,  By the Book. Onstage, Alexis Field shocked and tickled audiences in NYC and around the country with her “tequila soaked” portrayal of Anastasia’s best friend, Katherine Kavanaugh in the critically acclaimed 50 Shades the Musical. She has been seen in the NYMF production of Wearing Black and also appeared Off-Broadway and on tour in Angelina Ballerina. Other favorite roles include Percy in The Spitfire Grill, Diwata in Speech and Debate, Heidi in [tile of show], and Dorine in Tartuffe. Alexis has written and performed a one-woman cabaret called Tindering Across America, a saucy romp through sex, swiping, and song, which premiered at 54 Below. She has also had the pleasure of singing at the Lincoln Center, Joe’s Pub, and the Beechman Theatre. Alexis has studied both standard and musical improv with the Magnet Theatre and has been lucky enough to perform as a sketch house team member.  When Alexis isn't on stage or in front of a camera you can find her on a yoga mat, or singing Rock-N-Roll music to babies in NYC, or possibly eating something delicious and usually in awe of the world around her. </p>
-  //     <div>
-  //       <a href="https://twitter.com/lexinthecity1?lang=en" target="_blank" rel="noopener noreferrer"><img alt="twitter link" style={{ width: '30px', height: '30px' }}src={Twitter} /></a>
-  //       <a href="https://www.instagram.com/lexinthecity1/" target="_blank" rel="noopener noreferrer"><img alt="instagram link" style={{ marginLeft: '25px', width: '30px', height: '30px' }}src={Instagram} /></a>
 
-  //       {/* <span><a href="https://twitter.com/lexinthecity1?lang=en"><img style={{ width: '20px', height: '20px' }}src={Twitter} />@lexinthecity1</a></span>
-  //       <span><a href="https://www.instagram.com/lexinthecity1/">@lexinthecity1</a></span> */}
-  //     </div> https://www.patterncooler.com/#a96b96c12Cd12Ce0f0g64hF3C73D8A73B9iDEj0k49lE5mE5
-  //   </div>
-
-  // </Layout>
   <div
     style={{
       backgroundImage: `url(${BackgroundImage})`,
@@ -34,7 +21,9 @@ export default () => (
       overflow: `scroll`,
     }}
   >
-    <Desktop><br /></Desktop>
+    <Desktop>
+      <br />
+    </Desktop>
     <div
       style={{
         backgroundColor: `white`,
@@ -47,15 +36,12 @@ export default () => (
         // padding: `1.25rem 1rem`,
       }}
     >
-
       <Desktop>
-        
-      <img
+        <img
           style={{ margin: `12px`, maxHeight: `400px`, float: `right` }}
           src={Pic2}
         />
       </Desktop>
-
       <h2>Stage Meets Screen</h2>
       <h2>A Virtual Camp for Young Performers!</h2>
       <br />
@@ -65,7 +51,6 @@ export default () => (
           src={Pic2}
         />
       </Mobile>
-      
       <p>
         As performers, it is of the utmost importance to be flexible and when
         things don’t go as planned, to quickly adjust and go on with the show!
@@ -73,7 +58,7 @@ export default () => (
         Screen: a virtual camp for young performers!
       </p>
       <p>
-        Over the course of two weeks, your child will be digitally immersed in
+        In Stage Meets Screen Camp, your child will be digitally immersed in
         an online class with NYC-based Actor and Musical Theatre Educator,
         Alexis Field. Together they’ll sing, dance, play, and even meet working
         professionals in theater and television!
@@ -84,7 +69,7 @@ export default () => (
         - and have a total blast doing it!
       </p>
       <p>
-        Class sizes will be limited to 10 students so that each performer will
+        Camp will be limited to 10 students so that each performer will
         get their chance to shine!
       </p>
       <div
@@ -100,53 +85,48 @@ export default () => (
           color: `white`,
         }}
       >
-        Two Weeks of in-depth coaching, mentorship, and fun to bring out the
+        In-depth coaching, mentorship, and fun to bring out the
         best in your superstar!!
       </div>
-      <iframe style={{
-        margin: `0 auto`,
-        display: `block`,
-        maxWidth: `100%`
-      }} src="https://player.vimeo.com/video/426590141" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-
-
-      <u>Future Stars:</u>
+      <iframe
+        style={{
+          margin: `0 auto`,
+          display: `block`,
+          maxWidth: `100%`,
+        }}
+        src="https://player.vimeo.com/video/441486399"
+        width="640"
+        height="360"
+        frameborder="0"
+        allow="autoplay; fullscreen"
+        allowfullscreen
+      ></iframe>
+      <u>STARS:</u>
       <br />
-      Going into 1st - 5th Grade
-      <p>Meets: June 15th, 17th, 19th, 22nd, 24th, 26th (12:30pm-3pm EST)</p>
+      Going into 3rd - 8th Grade
+      <p>Meets: August 3rd, 4th, 5th, 6th and 7th</p>
       <p>
-        In this jampacked and dynamic 2.5 hour session, our younger group will
+        In this jampacked and dynamic 2.5 hour session, we will
         learn the basics of song and dance while collaborating on a group
         musical number! Our 10 lucky campers will play games to build skills,
         promote confidence, and learn basics of performing all while connecting
-        with their fellow campers. Our little performers will also receive
+        with their fellow campers. Our performers will also receive
         materials so they can rehearse on their own. Each day, we’ll begin with
         both a vocal and physical warm up to build range and skills. Improv
         games will be taught to help our stars learn to respond swiftly,
-        organically, and not take anything too seriously. Every day will end
-        with a Q&A where they’ll get to chat with an actual designer, casting
+        organically, and not take anything too seriously. Every day will include a Q&A session where campers will get to chat with an actual designer, casting
         director, Broadway actor, composer, or other industry professional. Our
-        two weeks will culminate in a group music video performance that will
+        program will culminate in a group music video performance that will
         commemorate our special time together!
       </p>
-      <u>Future Legends:</u>
+      <p></p>
+      <u>Potential Breakout Session for Middle School Campers:</u>
       <br />
-      Going into 6th-9th Grade
-      <p>Meets: June 15th, 17th, 19th, 22nd, 24th, 26th (2:30pm - 5pm EST)</p>
       <p>
-        In this older group, actors will begin each day with a Q&A where they’ll
-        get to chat with an actual designer, casting director, Broadway actor,
-        composer, or other industry professional. They’ll learn the fundamentals
-        of theatre through song, dance, and games much like the younger group
-        but they’ll also take part in an immersive “song study”. Each “Future
-        Legend” will get the opportunity to collaborate with Miss Alexis on the
-        first day, and based on these initial discussions, they will then be
-        assigned their very own short song that fits their personality and vocal
-        range. Throughout the sessions, Miss Alexis will coach each actor
-        through their solo to help them connect with the material and create
-        their own unique performance!
+      If 3 or more middle schoolers enroll I will craft a secondary performance opportunity and offer additional coaching from 3-3:30pm. This will remain TBD for now. 
       </p>
-      <b>Pricing:</b> $325*
+      <br />
+      <b>Pricing:</b> <span style={{ marginRight: "22px", textDecoration: "line-through" }}>$325</span>  <b>$250 condensed course*</b>
       <br />
       <p>
         Class sizes will be limited to 10 students, so register now to reserve
@@ -254,7 +234,7 @@ export default () => (
           A device to record your final performance (such as a smart phone) -
           for our music video
         </li>
-        <li>Water</li>
+        <li>Water with a reusable straw</li>
       </ul>
     </div>
   </div>
